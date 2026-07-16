c2c4ea4b2d310cde3b98afbd30414eb2436ab275
Naz <bluspringindohq@gmail.com>
2026-07-16 15:26:12 +0800

    mark as functional so people can use these in lambdas

--- api/src/main/kotlin/xyz/bluspring/unitytranslate/api/v2/transcriber/processor/TranscriptPostProcessor.kt
+++ api/src/main/kotlin/xyz/bluspring/unitytranslate/api/v2/transcriber/processor/TranscriptPostProcessor.kt
@@ -2,6 +2,6 @@ package xyz.bluspring.unitytranslate.api.v2.transcriber.processor
 
 import xyz.bluspring.unitytranslate.api.v2.Language
 
-interface TranscriptPostProcessor {
+fun interface TranscriptPostProcessor {
     fun processFinalTranscript(text: String, originalText: String, sourceLanguage: Language, currentLanguage: Language): String
 }
--- api/src/main/kotlin/xyz/bluspring/unitytranslate/api/v2/transcriber/processor/TranscriptPreProcessor.kt
+++ api/src/main/kotlin/xyz/bluspring/unitytranslate/api/v2/transcriber/processor/TranscriptPreProcessor.kt
@@ -2,6 +2,6 @@ package xyz.bluspring.unitytranslate.api.v2.transcriber.processor
 
 import xyz.bluspring.unitytranslate.api.v2.Language
 
-interface TranscriptPreProcessor {
+fun interface TranscriptPreProcessor {
     fun processTranscript(text: String, originalText: String, language: Language): String
 }
